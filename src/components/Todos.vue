9cdce99a74e15b67a9a15a1bd568e725294026b5
Mohamed Guerroui <guerroui.m@gmail.com>
2020-10-31 10:41:00 +0100

    ajout css et todos.vue

---
+++ src/components/Todos.vue
@@ -0,0 +1,42 @@
+<template>
+    <section class="todoapp">
+        <header class="header">
+            <h1>Todos</h1>
+            <input type="text" class="new-todo" placeholder="Ajouter une tâche" v-model="newTodo" @keyup.enter="addTodo">
+        </header>
+        <div class="main">
+            <ul class="todo-list">
+                <li class="todo" v-for="todo in todos">
+                    <div class="view">
+                        <label>{{ todo.name }}</label>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            todos: [{
+                name: 'Tache de test',
+                completed: false
+            }],
+            newTodo: ''
+        }
+    },
+    methods: {
+        addTodo () {
+            this.todos.push({
+                completed: false,
+                name: this.newTodo
+            })
+            this.newTodo = ''
+        }
+    }
+    
+}
+</script>
+<style src="./todos.css"></style>
