7f8ee968b5decda30d2f4bb2b11175eb618d94f7
Mohamed Guerroui <guerroui.m@gmail.com>
2020-11-02 16:12:54 +0100

    ajout du carousel et des boutons next/preview

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <carousel>
+    <carousel class="carousel">
       <carousel-slide>
         salut les gens
+        <img src="https://picsum.photos/630/300">
       </carousel-slide>
       <carousel-slide>
         Aurevoir les gens
+        <img src="https://picsum.photos/630/300">
       </carousel-slide>
     </carousel>
     <todos v-model="todos"></todos>
@@ -42,4 +44,26 @@ export default {
 </script>
 
 <style>
+  .carousel {
+    position: relative;
+    text-align: center;
+  }
+
+  .carousel__nav {
+    position: absolute;
+    top: 50%;
+    margin-top: -31px;
+    left: 10px;
+    background: url(components/carousel/prev.png);
+    width: 63px;
+    height: 63px;
+  }
+
+  .carousel__nav.carousel__next {
+    right: 10px;
+    left: auto;
+    background: url(components/carousel/next.png);
+
+  }
+
 </style>
--- src/components/carousel/Carousel.vue
+++ src/components/carousel/Carousel.vue
@@ -1,11 +1,41 @@
 <template>
     <div>
         <slot></slot>
+        <button class="carousel__nav carousel__next" @click.prevent="next"></button>
+        <button class="carousel__nav carousel__prev" @click.prevent="prev"></button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data () {
+        return {
+            index: 0,
+            slides: []
+        }
+    },
+    mounted () {
+        this.slides = this.$children
+        this.slides.forEach((slide, i) => {
+            slide.index = i
+        })
+    },
+    computed: {
+        slideCount () { return this.slides.length }
+    },
+    methods: {
+        next () {
+            this.index++
+            if (this.index >= this.slideCount) {
+                this.index = 0
+            }
+        },        
+        prev () {
+            this.index--
+            if (this.index < 0 ) {
+                this.index = this.slideCount - 1
+            }
+        },
+    }
 }
 </script>
